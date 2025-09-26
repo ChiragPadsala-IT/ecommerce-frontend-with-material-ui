c7e738eab0cace9a184d1a395b8468c0b3d1570b
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-09-26 17:06:58 -0400

    login page update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -9,9 +9,37 @@ import {
 } from "@mui/material";
 import googleLogo from "../Assets/Logos/google_logo.png";
 import facebookLogo from "../Assets/Logos/facebook_logo.png";
-import React from "react";
+import loginBg from "../Assets/Images/loginbg.webp";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [userCredential, setUserCredential] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onUserCredentialHandler = (e) => {
+    e.preventDefault();
+
+    const { name, value } = e.target;
+
+    setUserCredential((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(userCredential);
+
+    setUserCredential({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <>
       <Box
@@ -20,18 +48,21 @@ const Login = () => {
           justifyContent: "center",
           alignItems: "center",
           height: "100vh",
+          width: "100%",
         }}
       >
         <Box
           sx={{
-            width: { xs: "100vh", sm: "500px" },
-            //   background: "yellow",
+            width: { xs: "100vh", sm: "400px" },
             boxShadow: "5px 5px 20px ",
             borderRadius: "10px",
             padding: "30px 20px",
             display: "flex",
             flexDirection: "column",
             gap: 2,
+            bgcolor: "rgba(255, 255, 255, 0.9)",
+            zIndex: 100,
+            my: "auto",
           }}
         >
           <Typography
@@ -41,11 +72,23 @@ const Login = () => {
           </Typography>
           <FormControl variant="outlined">
             <InputLabel>Email</InputLabel>
-            <OutlinedInput label="Email" type="email" />
+            <OutlinedInput
+              label="Email"
+              type="email"
+              name="email"
+              value={userCredential.email}
+              onChange={onUserCredentialHandler}
+            />
           </FormControl>
           <FormControl variant="outlined">
             <InputLabel>Password</InputLabel>
-            <OutlinedInput label="Password" type="password" />
+            <OutlinedInput
+              label="Password"
+              type="password"
+              name="password"
+              value={userCredential.password}
+              onChange={onUserCredentialHandler}
+            />
           </FormControl>
           <Typography
             sx={{
@@ -56,7 +99,9 @@ const Login = () => {
           >
             Forgot Password?
           </Typography>
-          <Button variant="contained">Login</Button>
+          <Button variant="contained" type="submit" onClick={onSubmitHandler}>
+            Login
+          </Button>
           <Typography>
             Not Registered?{" "}
             <span
@@ -76,6 +121,12 @@ const Login = () => {
           </Box>
         </Box>
       </Box>
+      <img
+        height="100%"
+        width="100%"
+        style={{ position: "absolute", zIndex: 0, left: "0%", top: "0%" }}
+        src={loginBg}
+      />
     </>
   );
 };
